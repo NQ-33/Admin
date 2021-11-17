62acde5f8c05e7bc94013a33f08fd6686fd8987c
NQ-33 <1365591700@qq.com>
2021-11-18 01:26:25 +0800

    day1

--- src/main.js
+++ src/main.js
@@ -2,11 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
+import './assets/css/global.css'
+import './assets/css/fonts/iconfont.css'
+
+import element from './elementPartUI'
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(element)
 Vue.config.productionTip = false
 
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
